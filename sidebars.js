bb7b7ea539c8a548480a62a35d2dd375ce0a803e
Ankit Nayan <ankit@signoz.io>
2021-01-06 01:09:43 +0530

    added configuration in docs

--- sidebars.js
+++ sidebars.js
@@ -1,6 +1,7 @@
 module.exports = {
   someSidebar: {
     SigNoz: ['introduction','installation', 'architecture','instrumentation','contributing','community','roadmap','faq'],
+    Configuration: ['configuration/deep_storage', 'configuration/retention_period'],
     Features: ['mdx'],
   },
 };
