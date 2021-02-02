81333bd101d2880ef9b3ade61353952705ebb81c
Himanshu Dixit <hudixt@gmail.com>
2021-02-02 17:48:49 +0530

    Add open source tab

--- src/pages/pricing.js
+++ src/pages/pricing.js
@@ -8,13 +8,13 @@ const TABS = {
     OPEN_SOURCE: "Open source"
 };
 
-function TabsHeader(props){
+function TabsHeader({onSelectTab}){
 
      const items = (<>
-                <li className={"tabs__item tabs__item--active"}>Cloud</li>
-                <Link href={"/docs"}>
-                    <li className={"tabs__item"}>Open Source</li>
-                </Link>
+                <li className={"tabs__item tabs__item--active"} onClick={onSelectTab.bind(this,TABS.CLOUD)}>Cloud</li>
+
+                    <li className={"tabs__item"} onClick={onSelectTab.bind(this,TABS.OPEN_SOURCE)}>Open Source</li>
+
                 </>)
 
     return (
@@ -64,22 +64,62 @@ function PricingCard(props) {
     )
 }
 
+function OpenSourceCard(props) {
+    const {title, image, price, buttonText, features} = props;
+    const featuresListItems = features.map(feature => {
+        return (<li key={feature}>{feature}</li>);
+    })
+
+    const onButtonClick = ()=>{
+        window.location = `https://app.signoz.io?plan_type=${title.toLowerCase()}`
+    }
+
+    return (
+        <div className="card-demo" style={{background: "#fff", color: "#333333", borderRadius: "0.4rem", height: "100%"}}>
+            <div className="card" style={{background: "#fff", color: "#333333", height: "100%"}}>
+                <div className="card__header">
+                    <h3 style={{textAlign: "center"}}>{title}</h3>
+                </div>
+                <div className="card__image" style={{display: "flex", justifyContent: "center"}}>
+                    <img
+                        src={image}
+                        style={{height: "5.25rem"}}
+                        alt="Image alt text"
+                        title="Logo Title Text 1"
+                    />
+                </div>
+                <div className="card__body margin-vert--lg">
+                    <Conditional If={price}>
+                        <h4 style={{textAlign: "center"}}>{price}</h4>
+                    </Conditional>
+                    <ul>
+                        {featuresListItems}
+                    </ul>
+                </div>
+                <div className="card__footer">
+                    <button style={{background: "#2D9CDB", borderWidth: 0}} className="button button--primary button--block" onClick={onButtonClick}>{buttonText}</button>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+
 const HOBBY_FEATURES = ["Upto 100 mn events", "3 day retention", "Community Support"];
 const STARTER_FEATURES = ["Upto 100 mn events", "3 day retention", "Community Support"];
 const PROFESSIONAL_FEATURES = ["3 day retention - USD 0.1/mn spans", "7 day retention - USD 0.25/mn spans", "14 day retention - USD 0.45/mn spans", "30 day retention - USD 0.8/mn spans"];
+const OPEN_SOURCE = ["For people happy to manage their own infrastructure", "Community support"];
 
-function pricing() {
+function Pricing() {
     const [selectedTab, setSelectedTab] = useState(TABS.CLOUD);
-    const handleSelectTab = (tab) => {
-        setSelectedTab(tab);
-    };
+
     return (
         <Layout title="Pricing">
             <section>
                 <div className="container" style={{marginTop: '2rem', marginBottom:'4rem'}}>
                     <div className={"row"}>
                         <div className={"pricingTabsContainer col col--12"} style={{display: "flex", justifyContent:"center"}}>
-                            <TabsHeader onSelectTab={handleSelectTab} selectedTab={selectedTab}/>
+                            <TabsHeader onSelectTab={setSelectedTab} selectedTab={selectedTab}/>
                         </div>
                     </div>
                     <div className={"margin-vert--lg"}>
@@ -96,6 +136,14 @@ function pricing() {
                             </div>
                         </div>
                     </Conditional>
+
+                        <Conditional If={selectedTab === TABS.OPEN_SOURCE}>
+                            <div className={"row"} style={{display: "flex", justifyContent: "center"}}>
+                                <div className={"col col--4 margin-vert--md"}>
+                                    <OpenSourceCard title={"Free"} image={"/img/hobby-pricing.png"} price={"Free"} features={OPEN_SOURCE} buttonText={"Get started for free"}/>
+                                </div>
+                            </div>
+                        </Conditional>
                     </div>
                 </div>
             </section>
@@ -103,4 +151,4 @@ function pricing() {
     );
 }
 
-export default pricing;
+export default Pricing;
