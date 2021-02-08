53942820ae3a460ee89bcee978acc6dc8ae6c3b6
Pranay Prateek <pranay@signoz.io>
2021-02-08 13:51:09 +0530

    updated faq

--- sidebars.js
+++ sidebars.js
@@ -1,6 +1,6 @@
 module.exports = {
   someSidebar: {
-    SigNoz: ['introduction', 'architecture', 'features','contributing','community','roadmap','faq'],
+    Overview: ['introduction', 'architecture', 'features','contributing','community','roadmap','faq'],
     Deployment: ['deployment/docker', 'deployment/helm_chart'],
     Configuration: ['configuration/deep_storage', 'configuration/retention_period'],
     Instrumentation: ['instrumentation/overview','instrumentation/python', 'instrumentation/nodejs'],
