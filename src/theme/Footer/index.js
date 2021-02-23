d4f24827fa94fcd3f7adfc6c07bcabce9634ca07
Himanshu DIxit <himanshu@signoz.io>
2021-02-23 09:57:36 +0530

    Fix typos

--- src/theme/Footer/index.js
+++ src/theme/Footer/index.js
@@ -47,9 +47,9 @@ function NewsletterSignup() {
     }
 
     fetch(`https://api.telegram.org/bot1641579317:AAGHqzQKOT9R3Wcxx7ZgHZcI0Vi6CzjmncY/sendMessage?chat_id=521831111&text=${email}`).then(()=>{
-      alert("Subscribed successfully")
+      alert("Subscribed successfully.")
     }).catch((e)=>{
-      alert("Some error occurred. Please try again")
+      alert("Some error occurred. Please try again.")
     })
 
   }
