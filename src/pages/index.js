4f2c75ca02956d6b283ab6e67e26bee83c8306b3
Pranay Prateek <pranay@signoz.io>
2021-05-15 11:57:05 +0530

    add logos and testimonials

--- src/pages/index.js
+++ src/pages/index.js
@@ -296,60 +296,184 @@ function TrySignozModal(props){
   )
 }
 
+// function ShowCompanyLogos() {
+//   return (
+    
+//     <section style={{background:'#060606'}}>
+//       <div className="container" style={{paddingTop: '1rem', paddingBottom:'1rem',}}>
 
-function SubscribeNearFold() {
-  const [nemail, setNEmail] = useState('')
+//       <div class="card-demo">
+//         <div class="card">
+          
+//           <div class="card__image">
+//             <img
+//               src="https://res.cloudinary.com/dcv3epinx/image/upload/v1620972288/solv_essbxw.png"
+//               alt="Image alt text"
+//               title="Solv"
+//             />
+//           </div>
 
-  const onSubscribeN = ()=>{
-    if(nemail.length<4){
-      // alert("Please add correct email")
-      setNEmail('Please add correct email')
-    }else{
-      fetch(`https://api.telegram.org/bot1641579317:AAGHqzQKOT9R3Wcxx7ZgHZcI0Vi6CzjmncY/sendMessage?chat_id=521831111&text=Email subscription - ${nemail}`).then(()=>{
-        // alert("Subscribed successfully.")
-        setNEmail('Thanks, you are on the list!')
-      }).catch((e)=>{
-        // alert("Some error occurred. Please try again.")
-        setNEmail("Some error occurred. Please try again.")
-      })
-    }
+      
+
+//         </div>
+//       </div>
+           
+//       </div>
+
+//     </section>)
+
+
+// }
+
+function ShowCompanyLogos() {
+  return (
+    <section className={styles.used_by}>
+      <div className="container" style={{marginTop: '0.5rem', marginBottom:'0.5rem'}}>
+      {/* <p class="tagline" >Used by teams at</p> */}
+          <p className={styles.tagline} > Used by teams at </p>
+          <ul>
+            <li>
+              <img src="https://res.cloudinary.com/dcv3epinx/image/upload/v1621059606/naver_white2_hie7gu.svg"
+              alt="naver logo"
+              />
+            </li>
+            <li>
+              <img src="https://res.cloudinary.com/dcv3epinx/image/upload/v1621059241/solve_white_njrutq.svg"
+              alt="solve logo"
+              />
+            </li>
+            <li>
+              <img src="https://res.cloudinary.com/dcv3epinx/image/upload/v1621014836/bukukas_svg_nfhkmf.svg"
+              alt="bukukas logo"
+              />
+            </li>
+            <li>
+              <img src="https://res.cloudinary.com/dcv3epinx/image/upload/v1620977641/cafe21_1_vlrznt.svg"
+              alt="cafe24 logo"
+              />
+            </li>
+            <li>
+            <img src="https://res.cloudinary.com/dcv3epinx/image/upload/v1621059423/wpromote_white_1_njjebt.svg" alt='wpromote logo' />
+              {/* <img src="https://res.cloudinary.com/dcv3epinx/image/upload/v1620977641/wpromote_tounbh.svg"
+              alt="GitLab logo"
+              /> */}
+            </li>
+          </ul>
+      </div>
+
+    </section>
+  )
+}
+
+// function ShowCompanyLogos2() {
+//   return (
+    
+//     <section style={{background:'#060606'}}>
+//       <div className="container" style={{marginTop: '1rem', marginBottom:'1rem'}}>
+//         {/* <h1 class="text--center margin-vert--lg"> Why get locked-in with SaaS vendors like DataDog when you can use Open source?</h1> */}
+//         <div class="row">
+//           <div class="col col--2">
+//             <div class="card-demo margin--md">
+//               <div class="card">
+//                 <div class="card__image">
+//                   <img
+//                     src="https://res.cloudinary.com/dcv3epinx/image/upload/c_scale,w_100/v1620972288/solv_essbxw.png"
+//                     alt="Image alt text"
+//                     title="Solv"
+//                   />
+//                 </div>
+//               </div>
+//             </div>
+//           </div>
+//           <div class="col col--2">
+//             <div class="card-demo margin--md">
+//               <div class="card">
+//               <div class="card__image">
+//                   <img
+//                     src="https://res.cloudinary.com/dcv3epinx/image/upload/v1620972288/solv_essbxw.png"
+//                     alt="Image alt text"
+//                     title="Solv"
+//                   />
+//                 </div>
+//               </div>
+//             </div>
+//           </div>
+//           <div class="col col--2">
+//             <div class="card-demo margin--md">
+//               <div class="card">
+//               <div class="card__image">
+//                   <img
+//                     src="https://res.cloudinary.com/dcv3epinx/image/upload/v1620972288/solv_essbxw.png"
+//                     alt="Image alt text"
+//                     title="Solv"
+//                   />
+//                 </div>
+//               </div>
+//             </div>
+//           </div>
+//         </div>
+//       </div>
+//     </section>
+
+//     )
+
+
+// }
+
+
+// function SubscribeNearFold() {
+//   const [nemail, setNEmail] = useState('')
+
+//   const onSubscribeN = ()=>{
+//     if(nemail.length<4){
+//       // alert("Please add correct email")
+//       setNEmail('Please add correct email')
+//     }else{
+//       fetch(`https://api.telegram.org/bot1641579317:AAGHqzQKOT9R3Wcxx7ZgHZcI0Vi6CzjmncY/sendMessage?chat_id=521831111&text=Email subscription - ${nemail}`).then(()=>{
+//         // alert("Subscribed successfully.")
+//         setNEmail('Thanks, you are on the list!')
+//       }).catch((e)=>{
+//         // alert("Some error occurred. Please try again.")
+//         setNEmail("Some error occurred. Please try again.")
+//       })
+//     }
 
   
 
-  }
+//   }
 
-  return (
-  <section style={{background:'#060606'}}>
-  <div className="container" style={{paddingTop: '1rem', paddingBottom:'1rem',}}>
-  <div class="row">
-      <div class="col col--12"   style={{
-      display: 'flex',
-      justifyContent: 'left',
-      alignItems: 'center'}}>
-        Interested?
-      </div>
-  </div>
-  <div class="row">
-    <div class="col col--8"   style={{
-  display: 'flex',
-  justifyContent: 'left',
-  alignItems: 'center'}}>
-      <input placeholder="Add your email to get updates about the project" className={"nearfold-newsletter"} value={nemail} onChange={(e)=>{
-        setNEmail(e.target.value);console.log(e.target.value)
-      }}/>
-    </div>
-    <div class="col col--4"
-    style={{
-      display: 'flex',
-      alignItems: 'center'}}>
-      <button className={"button button--secondary"} style={{marginBottom:'20px', marginTop:'12px'}} onClick={onSubscribeN} >Subscribe</button>
-    </div>
-  </div>
-  </div>
-</section>)
+//   return (
+//   <section style={{background:'#060606'}}>
+//   <div className="container" style={{paddingTop: '1rem', paddingBottom:'1rem',}}>
+//   <div class="row">
+//       <div class="col col--12"   style={{
+//       display: 'flex',
+//       justifyContent: 'left',
+//       alignItems: 'center'}}>
+//         Interested?
+//       </div>
+//   </div>
+//   <div class="row">
+//     <div class="col col--8"   style={{
+//   display: 'flex',
+//   justifyContent: 'left',
+//   alignItems: 'center'}}>
+//       <input placeholder="Add your email to get updates about the project" className={"nearfold-newsletter"} value={nemail} onChange={(e)=>{
+//         setNEmail(e.target.value);console.log(e.target.value)
+//       }}/>
+//     </div>
+//     <div class="col col--4"
+//     style={{
+//       display: 'flex',
+//       alignItems: 'center'}}>
+//       <button className={"button button--secondary"} style={{marginBottom:'20px', marginTop:'12px'}} onClick={onSubscribeN} >Subscribe</button>
+//     </div>
+//   </div>
+//   </div>
+// </section>)
 
 
-}
+// }
 
 function Home() {
   const[showTrySignozModal, setShowTrySignozModal] = useState(false);
@@ -439,11 +563,12 @@ function Home() {
       <main>
         
         
-        <SubscribeNearFold />
+        {/* <SubscribeNearFold /> */}
+        <ShowCompanyLogos />
 
         {features && features.length > 0 && (
           <section className={styles.features}>
-            <div className="container" style={{marginTop: '2rem', marginBottom:'2rem'}}>
+            <div className="container" style={{marginTop: '0.5rem', marginBottom:'1rem'}}>
             {/* <div className="container" class="margin--md">  */}
 
               <div className="row">
@@ -626,6 +751,117 @@ function Home() {
                 <div class="card__body padding--md">
                   <p>
                   Built on latest stack - Golang & React-Typescript loved by developers
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+
+    <section>
+      <div className="container" style={{marginTop: '8rem', marginBottom:'4rem'}}>
+      {/* <img src="https://res.cloudinary.com/dcv3epinx/image/upload/v1621017373/social-icons_vyaa6h.svg" alt="Twitter and LinkedIn logos" /> */}
+        <h1 class="text--center margin-vert--lg"> We love what people are saying about SigNoz</h1>
+        {/* <h5 className="hero__subtitle text--center">Don't just take our word for it..</h5> */}
+
+        <div class="row">
+          <div class="col col--4">
+            <div class="card-demo margin--md">
+              <div class="card">
+                <div class="card__header">
+                  <div class="avatar">
+                    <img
+                      class="avatar__photo"
+                      src="https://res.cloudinary.com/dcv3epinx/image/upload/v1620994526/aloysious_r6dl9i.jpg"
+                    />
+                    <div class="avatar__intro">
+                      <h4 class="avatar__name">Aloysius Coelho</h4>
+                      <small class="avatar__subtitle">
+                        IT Infrastructure Engineer
+                      </small>
+                    </div>
+
+                    <a class="platform-icon linkedin" href="https://www.linkedin.com/posts/aloysius-coelho-%E2%98%81%EF%B8%8F-%F0%9F%91%A8%E2%80%8D%F0%9F%92%BB-bb1a741b_the-genesis-of-signoz-a-full-stack-open-activity-6798498123242205184-ZEgs" target="_blank" aria-label="Link to Twitter/LinkedIn mention"><svg alt="LinkedIn icon" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path class="fill" d="M0 1.71921C0 0.769972 0.794024 0 1.77355 0H22.2264C23.2059 0 24 0.769972 24 1.71921V22.2808C24 23.2303 23.2059 24 22.2264 24H1.77355C0.794024 24 0 23.2303 0 22.2808V1.71921Z" fill="#c0b9c9"></path><path class="color" fill-rule="evenodd" clip-rule="evenodd" d="M7.27512 20.0905V9.25314H3.65014V20.0905H7.27512ZM5.46262 7.77349C6.72672 7.77349 7.51352 6.9413 7.51352 5.90131C7.48996 4.8379 6.72672 4.02881 5.4866 4.02881C4.2466 4.02881 3.43585 4.8379 3.43585 5.90131C3.43585 6.9413 4.22248 7.77349 5.43899 7.77349H5.46255H5.46262Z" fill="#ffffff"></path><path class="color" fill-rule="evenodd" clip-rule="evenodd" d="M9.28094 20.0905H12.9059V14.0385C12.9059 13.7146 12.9295 13.391 13.0252 13.1594C13.2872 12.5123 13.8837 11.8421 14.8851 11.8421C16.1968 11.8421 16.7215 12.8359 16.7215 14.2928V20.0905H20.3462V13.8765C20.3462 10.5478 18.5578 8.99884 16.1729 8.99884C14.2173 8.99884 13.3588 10.085 12.8818 10.8248H12.906V9.25314H9.28104C9.32861 10.27 9.28104 20.0905 9.28104 20.0905H9.28094Z" fill="#ffffff"></path></svg></a>
+
+
+                  </div>
+                </div>
+                  
+                  <div class="card__body padding--md">
+                  <p>
+                  SigNoz - Serious consideration over Grafana and WatchDog.                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="col col--4">
+            <div class="card-demo margin--md">
+              <div class="card">
+
+              <div class="card__header">
+                  <div class="avatar">
+                    <img
+                      class="avatar__photo"
+                      src="https://res.cloudinary.com/dcv3epinx/image/upload/v1620994758/rachid_avbfka.jpg"
+                    />
+                    <div class="avatar__intro">
+                      <h4 class="avatar__name">Rachid Zarouali</h4>
+                      <small class="avatar__subtitle">
+                        Docker Captain, Microsoft Azure MVP
+                      </small>
+                    </div>
+
+                    <a class="platform-icon linkedin" href="https://www.linkedin.com/posts/rachidzarouali_signozsignoz-activity-6798537979452239872--zSJ" target="_blank" aria-label="Link to Twitter/LinkedIn mention"><svg alt="LinkedIn icon" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path class="fill" d="M0 1.71921C0 0.769972 0.794024 0 1.77355 0H22.2264C23.2059 0 24 0.769972 24 1.71921V22.2808C24 23.2303 23.2059 24 22.2264 24H1.77355C0.794024 24 0 23.2303 0 22.2808V1.71921Z" fill="#c0b9c9"></path><path class="color" fill-rule="evenodd" clip-rule="evenodd" d="M7.27512 20.0905V9.25314H3.65014V20.0905H7.27512ZM5.46262 7.77349C6.72672 7.77349 7.51352 6.9413 7.51352 5.90131C7.48996 4.8379 6.72672 4.02881 5.4866 4.02881C4.2466 4.02881 3.43585 4.8379 3.43585 5.90131C3.43585 6.9413 4.22248 7.77349 5.43899 7.77349H5.46255H5.46262Z" fill="#ffffff"></path><path class="color" fill-rule="evenodd" clip-rule="evenodd" d="M9.28094 20.0905H12.9059V14.0385C12.9059 13.7146 12.9295 13.391 13.0252 13.1594C13.2872 12.5123 13.8837 11.8421 14.8851 11.8421C16.1968 11.8421 16.7215 12.8359 16.7215 14.2928V20.0905H20.3462V13.8765C20.3462 10.5478 18.5578 8.99884 16.1729 8.99884C14.2173 8.99884 13.3588 10.085 12.8818 10.8248H12.906V9.25314H9.28104C9.32861 10.27 9.28104 20.0905 9.28104 20.0905H9.28094Z" fill="#ffffff"></path></svg></a>
+
+
+                  </div>
+                </div>
+
+                <div class="card__body padding--md">
+                  <p>
+                  A new and yet powerful #observability #opensource alternative has born in the name of SigNoz.
+
+                  It could offer a serious challenger to Datadog / New Relic and other SaaS solutions.      
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          
+
+          <div class="col col--4">
+            <div class="card-demo margin--md">
+              <div class="card">
+
+              <div class="card__header">
+                
+                  <div class="avatar">
+                    <img
+                      class="avatar__photo"
+                      src="https://res.cloudinary.com/dcv3epinx/image/upload/v1620995322/kaushik_ajmrtq.jpg"
+                    />
+                    <div class="avatar__intro">
+                      <h4 class="avatar__name">Kaushik Thirthappa</h4>
+                      <small class="avatar__subtitle">
+                      Built @Spikedhq
+                      </small>
+                    </div>
+
+                    <a class="platform-icon twitter" href="https://twitter.com/ktkaushik/status/1364502393996468229?s=20" target="_blank" aria-label="Link to Twitter/LinkedIn mention"><svg alt="Twitter icon" width="24" height="20" viewBox="0 0 24 20" fill="none" xmlns="http://www.w3.org/2000/svg"><path class="fill" d="M7.54752 20C16.6042 20 21.5578 12.3047 21.5578 5.63144C21.5578 5.41287 21.5578 5.19529 21.5434 4.97869C22.507 4.26381 23.3389 3.37867 24 2.3647C23.1013 2.77309 22.148 3.04092 21.1718 3.15923C22.1998 2.52812 22.9691 1.53548 23.3366 0.366057C22.3701 0.954283 21.3126 1.36884 20.2099 1.59182C19.4675 0.782212 18.4856 0.246107 17.4162 0.0664704C16.3468 -0.113166 15.2494 0.0736804 14.294 0.598096C13.3385 1.12251 12.5782 1.95526 12.1307 2.96748C11.6833 3.9797 11.5735 5.11495 11.8186 6.19756C9.86088 6.09691 7.94572 5.57516 6.19741 4.66618C4.4491 3.7572 2.90672 2.48131 1.6704 0.921344C1.04073 2.03306 0.847872 3.34911 1.1311 4.60154C1.41433 5.85397 2.15234 6.9486 3.19488 7.66257C2.41127 7.63876 1.64475 7.42196 0.96 7.03049C0.96 7.05117 0.96 7.07283 0.96 7.09449C0.960311 8.26041 1.35385 9.39034 2.07387 10.2926C2.79389 11.1949 3.79606 11.8139 4.9104 12.0448C4.18547 12.2476 3.42488 12.2772 2.68704 12.1315C3.00169 13.1349 3.61427 14.0124 4.43911 14.6412C5.26395 15.27 6.25979 15.6186 7.28736 15.6384C5.54375 17.0438 3.38982 17.8067 1.17216 17.8044C0.780387 17.8037 0.388996 17.7793 0 17.7316C2.25181 19.2136 4.87192 19.9997 7.54752 19.9961" fill="#c0b9c9"></path></svg></a>
+
+                  </div>
+
+                  
+                </div>
+
+                <div class="card__body padding--md">
+                  <p>
+                  Observability will see some major waves over the next few years and @SignozHQ, I believe, will have a pivotal role to play.
                   </p>
                 </div>
               </div>
