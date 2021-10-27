767e638f3b26dfdd5e24e5c8ea3ca248c778d3be
Pranay Prateek <pranay@signoz.io>
2021-10-28 00:27:38 +0530

    added more content cards and actual images

--- src/pages/user-stories.js
+++ src/pages/user-stories.js
@@ -20,7 +20,7 @@ function aboutus() {
                 <div class="card">
                   <div class="card__image">
                     <img
-                      src="https://images.unsplash.com/photo-1506624183912-c602f4a21ca7?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=crop&amp;w=800&amp;q=60"
+                      src="/img/user_stories/1_netflix_distributed_tracing.webp"
                       alt="Image alt text"
                       title="Logo Title Text 1"
                     />
@@ -32,8 +32,12 @@ function aboutus() {
                     </small>
                   </div>
                   <div class="card__footer">
-                    <button class="button button--primary button--block">Visit</button>
-                  </div>
+                    <Link
+                      className="button button--secondary button--outline"
+                      href={"https://netflixtechblog.com/building-netflixs-distributed-tracing-infrastructure-bb856c319304"}
+                    >
+                      Visit
+                    </Link>                  </div>
                 </div>
               </div>
             </div>
@@ -43,7 +47,7 @@ function aboutus() {
                 <div class="card">
                   <div class="card__image">
                     <img
-                      src="https://images.unsplash.com/photo-1506624183912-c602f4a21ca7?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=crop&amp;w=800&amp;q=60"
+                      src="/img/user_stories/2_netflix_lesson_learnt.webp"
                       alt="Image alt text"
                       title="Logo Title Text 1"
                     />
@@ -51,43 +55,220 @@ function aboutus() {
                   <div class="card__body">
                     <h4>Lessons from Building Observability Tools at Netflix</h4>
                     <small>
-                    5 key learnings of Netflix engineering team from building observability tools. The learnings cover scaling log ingestion, contextual distributed tracing, analysis of metrics, choosing observability database and data visualization.
-                    </small>
+                    5 key learnings of Netflix engineering team from building observability tools. Scaling log ingestion, contextual distributed tracing, analysis of metrics, choosing observability database and data visualization.                    </small>
                   </div>
                   <div class="card__footer">
-                    <button class="button button--primary button--block">Visit</button>
-                  </div>
+                    <Link
+                      className="button button--secondary button--outline"
+                      href={"https://netflixtechblog.com/lessons-from-building-observability-tools-at-netflix-7cfafed6ab17"}
+                    >
+                      Visit
+                    </Link>                  </div>
                 </div>
               </div>
              
             </div>
 
             <div class="col col--4">
-            <div class="card-demo">
-                <div class="card">
-                  <div class="card__image">
-                    <img
-                      src="https://images.unsplash.com/photo-1506624183912-c602f4a21ca7?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=crop&amp;w=800&amp;q=60"
-                      alt="Image alt text"
-                      title="Logo Title Text 1"
-                    />
-                  </div>
-                  <div class="card__body">
-                    <h4>Edgar: Solving Mysteries Faster with Observability</h4>
-                    <small>
-                      Edgar: Solving Mysteries Faster with Observability -yaddya yaadda yaddya
-                      Lorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem Ipsum
-                    </small>
-                  </div>
-                  <div class="card__footer">
-                    <button class="button button--primary button--block">Visit</button>
+              <div class="card-demo">
+                  <div class="card">
+                    <div class="card__image">
+                      <img
+                        src="/img/user_stories/3_netflix_edgar_solving_mysteries.webp"
+                        alt="Image alt text"
+                        title="Logo Title Text 1"
+                      />
+                    </div>
+                    <div class="card__body">
+                      <h4>Edgar: Solving Mysteries Faster with Observability</h4>
+                      <small>
+                      Author describes Edgar, a self-service tool for troubleshooting distributed systems, which also pulls in additional context from logs, events and metadata.
+                      </small>
+                    </div>
+                    <div class="card__footer">
+                      <Link
+                        className="button button--secondary button--outline"
+                        href={"https://netflixtechblog.com/edgar-solving-mysteries-faster-with-observability-e1a76302c71f"}
+                      >
+                        Visit
+                      </Link>
+                    </div>
                   </div>
                 </div>
-              </div>
-             
+              
+              </div>  
+
+          </div>
+
+
+          <div class="row">
+            <div class="col col--4">
+              <div class="card-demo">
+                  <div class="card">
+                    <div class="card__image">
+                      <img
+                        src="/img/user_stories/4_netflix_achieving_observability_async.webp"
+                        alt="Image alt text"
+                        title="Logo Title Text 1"
+                      />
+                    </div>
+                    <div class="card__body">
+                      <h4>Achieving observability in async workflows</h4>
+                      <small>
+                      In this article, Netflix engineering team describes how they built an observability framework for a content production facing application that uses asynchronous workflows.                      </small>
+                    </div>
+                    <div class="card__footer">
+                      <Link
+                        className="button button--secondary button--outline"
+                        href={"https://netflixtechblog.com/achieving-observability-in-async-workflows-cd89b923c784"}
+                      >
+                        Visit
+                      </Link>
+                    </div>
+                  </div>
+                </div>
+            </div>
+
+            <div class="col col--4">
+            <div class="card-demo">
+                  <div class="card">
+                    <div class="card__image">
+                      <img
+                        src="/img/user_stories/5_netflix_telltale_application_monitoring.webp"
+                        alt="Image alt text"
+                        title="Logo Title Text 1"
+                      />
+                    </div>
+                    <div class="card__body">
+                      <h4>Telltale: Netflix Application Monitoring Simplified</h4>
+                      <small>
+                      Netflix team describes Telltale, their application monitoring tool. Telltale monitors the health of over 100 Netflix production-facing applications with an intelligent alerting system.
+                      </small>
+                    </div>
+                    <div class="card__footer">
+                      <Link
+                        className="button button--secondary button--outline"
+                        href={"https://netflixtechblog.com/telltale-netflix-application-monitoring-simplified-5c08bfa780ba"}
+                      >
+                        Visit
+                      </Link>
+                    </div>
+                  </div>
+                </div>
+            </div>
+
+            <div class="col col--4">
+            <div class="card-demo">
+                  <div class="card">
+                    <div class="card__image">
+                      <img
+                        src="/img/user_stories/6_towards_observability_data.webp"
+                        alt="Image alt text"
+                        title="Logo Title Text 1"
+                      />
+                    </div>
+                    <div class="card__body">
+                      <h4>Towards Observability Data Management at Scale</h4>
+                      <small>
+                      This research paper explores the challenges and opportunities involved in designing and building Observability Data Management Systems. Written by authors from Brown University, MIT, Intel, and Slack.                      </small>
+                    </div>
+                    <div class="card__footer">
+                      <Link
+                        className="button button--secondary button--outline"
+                        href={"https://sigmodrecord.org/publications/sigmodRecord/2012/pdfs/05_Vision_Karumuri.pdf"}
+                      >
+                        Visit
+                      </Link>
+                    </div>
+                  </div>
+                </div>
             </div>
 
           </div>
+
+          <div class="row">
+            <div class="col col--4">
+              <div class="card-demo">
+                  <div class="card">
+                    <div class="card__image">
+                      <img
+                        src="/img/user_stories/7_monitoring_vs_observability.webp"
+                        alt="Image alt text"
+                        title="Logo Title Text 1"
+                      />
+                    </div>
+                    <div class="card__body">
+                      <h4></h4>
+                      <small>
+                      Cindy Sridharan explains the differences between monitoring and observability along with a brief overview of observability's origin. Read how observability complements monitoring.                      </small>
+                    </div>
+                    <div class="card__footer">
+                      <Link
+                        className="button button--secondary button--outline"
+                        href={"https://copyconstruct.medium.com/monitoring-and-observability-8417d1952e1c"}
+                      >
+                        Visit
+                      </Link>
+                    </div>
+                  </div>
+                </div>
+            </div>
+
+            <div class="col col--4">
+            <div class="card-demo">
+                  <div class="card">
+                    <div class="card__image">
+                      <img
+                        src="/img/user_stories/8_replaced_splunk.webp"
+                        alt="Image alt text"
+                        title="Logo Title Text 1"
+                      />
+                    </div>
+                    <div class="card__body">
+                      <h4>We Replaced Splunk at 100TB Scale in 120 Days</h4>
+                      <small>
+                      What do you do when your monitoring vendor becomes financially unviable and you're at 100TB daily ingestion volumes. Read on to find out how Groupon replaces Splunk in 120 days despite the scale.                      </small>
+                    </div>
+                    <div class="card__footer">
+                      <Link
+                        className="button button--secondary button--outline"
+                        href={"https://medium.com/lets-xplore/how-we-replaced-splunk-at-100tb-scale-in-120-days-e5a59db63f6"}
+                      >
+                        Visit
+                      </Link>
+                    </div>
+                  </div>
+                </div>
+            </div>
+
+            <div class="col col--4">
+            <div class="card-demo">
+                  <div class="card">
+                    <div class="card__image">
+                      <img
+                        src="/img/user_stories/9_observability_at_scale_uber.webp"
+                        alt="Image alt text"
+                        title="Logo Title Text 1"
+                      />
+                    </div>
+                    <div class="card__body">
+                      <h4>Observability at Scale: Building Uber’s Alerting Ecosystem</h4>
+                      <small>
+                      Find out how Uber's Observability team built a robust and scalable metrics and alerting pipeline responsible for detecting, notifying engineers of issues with their services as soon as they occur. </small>                    </div>
+                    <div class="card__footer">
+                      <Link
+                        className="button button--secondary button--outline"
+                        href={"https://eng.uber.com/observability-at-scale/"}
+                      >
+                        Visit
+                      </Link>
+                    </div>
+                  </div>
+                </div>
+            </div>
+
+          </div>
+          
         </div>
       </section>
     </Layout>
