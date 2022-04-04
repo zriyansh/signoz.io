1e3a914ec2ee6425cf81f0d3ae2137dacb488112
Palash gupta <palash@signoz.io>
2022-04-04 23:44:53 +0530

    onClickImage component is updated to tsx

--- src/components/onClickImage.tsx
+++ src/components/onClickImage.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import "react-medium-image-zoom/dist/styles.css";
 import Zoom from "react-medium-image-zoom";
 
-const onClickImage = ({ image, alt }) => (
+const onClickImage = ({ image, alt }): JSX.Element => (
   <Zoom zoomMargin={100}>
     <img src={image} alt={alt} />
   </Zoom>
