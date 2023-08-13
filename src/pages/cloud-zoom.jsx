95b4337cef7a8d5f5a363600363b145688a3e861
Pranay Prateek <pranay@signoz.io>
2023-08-13 23:43:51 +0530

    Added scheduling page after form submission

---
+++ src/pages/cloud-zoom.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import Layout from "@theme/Layout";
+import Head from '@docusaurus/Head';
+
+function cloudzoom() {
+  return (
+    <Layout title="Book a Call">
+    <Head>
+        <meta name="robots" content="noindex" />  
+        <script src="https://assets.calendly.com/assets/external/widget.js" type="text/javascript" async></script>
+    </Head>
+      <section>
+        <div
+          className="container"
+          style={{ marginTop: "8rem", marginBottom: "4rem" }}
+        >
+
+
+<div class="hero shadow--lw">
+  <div class="container">
+    <h1>Thanks for your interest in SigNoz Teams plan!</h1>
+    <p class="hero__subtitle"> An expert will reach out shortly to understand your requirements and get you started with your SigNoz cloud instance.
+    </p>
+
+    <p class="hero__subtitle">If you are interested in booking a call right away, feel free to book an available time slot from the below calendar.
+    </p>
+
+    <div class="calendly-inline-widget" 
+        data-url="https://calendly.com/pranay-signoz/signoz-intro-calls/" 
+        style={{minWidth:'320px', height:'700px'}}>
+    </div>
+
+  </div>
+</div>
+        
+
+         
+
+        </div>
+      </section>
+    </Layout>
+  );
+}
+
+export default cloudzoom;
