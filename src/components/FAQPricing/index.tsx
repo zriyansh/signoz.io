b0fc4277a98bfa8554d77305cfd13f61e3538f30
Ankit Anand <83692067+ankit01-oss@users.noreply.github.com>
2023-08-22 15:44:46 +0530

    Added differences between cloud and community in pricing faq (#1055)

--- src/components/FAQPricing/index.tsx
+++ src/components/FAQPricing/index.tsx
@@ -3,6 +3,11 @@ import Card from "./Card";
 
 const FAQs = [
   {
+    body: "You can self-host and manage the community edition yourself. You should choose SigNoz Cloud if you don’t want to worry about managing the SigNoz cluster. There are some exclusive features like unlimited dashboards for logs & traces and SSO & SAML support, which come with SigNoz cloud offering. Our team also offers support on the initial configuration of dashboards & alerts and advises on best practices for setting up your observability stack in the SigNoz cloud offering.",
+    title: "What is the difference between SigNoz cloud(Teams) and community edition?"
+
+  },
+  {
     body: "If a timeseries sends data every 30s, then it will generate 2 samples per min. So, if you have 10,000 time series sending data every 30s then you will be sending 20,000 samples per min to SigNoz. This will be around 864 mn samples per month and would cost 86.4 USD/month",
     title: "How are number of samples calculated for metrics pricing?",
   },
