821b9ab24c13abae9754db726ee7a9a50b499b95
Pranay Prateek <pranay@signoz.io>
2023-08-29 14:44:38 +0530

    reverting signup form

--- src/pages/cloud-zoom.jsx
+++ src/pages/cloud-zoom.jsx
@@ -18,21 +18,19 @@ function cloudzoom() {
 
 <div class="hero shadow--lw">
   <div class="container">
-    <h1>We have sent you a Verification Email</h1>
-    <p class="hero__subtitle"> 
-    Please click the Verify Email button to confirm your email.
-    <br />
-    After that, we'll send your SigNoz cloud account login details to your email.    </p>
-    <br />
-    <p class="hero__subtitle">Got a question about SigNoz? 
-    <br />
-    Pick a time from the calendar below to set up a call. We'll walk you through the product and address any queries you have.
+    
+  <h1>Thanks for your interest in SigNoz Teams plan!</h1>
+    <p class="hero__subtitle"> An expert will reach out shortly to understand your requirements and get you started with your SigNoz cloud instance.
+    </p>
+
+    <p class="hero__subtitle">If you have a specific question, feel free to book an available time slot from the below calendar. During the call, we will share a quick product demo and answer any questions you might have.
     </p>
 
     <div class="calendly-inline-widget" 
         data-url="https://calendly.com/pranay-signoz/signoz-intro-calls/" 
         style={{minWidth:'320px', height:'700px'}}>
     </div>
+
 
   </div>
 </div>
