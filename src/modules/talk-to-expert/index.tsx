eae9c36d119f9212b0d4a30fd2dfcf1f914bdfa8
Pranay Prateek <pranay@signoz.io>
2024-02-23 20:39:25 +0530

    Update index.tsx

--- src/modules/talk-to-expert/index.tsx
+++ src/modules/talk-to-expert/index.tsx
@@ -3,7 +3,7 @@ import Link from "@docusaurus/Link";
 import useBaseUrl from "@docusaurus/useBaseUrl";
 import styles from "./styles.module.css";
 
-const TALK_TO_EXPERT_URL = "https://calendly.com/pranay-signoz/signoz-talk-to-expert";
+const TALK_TO_EXPERT_URL = "https://share.hsforms.com/1kLv_9137QQSkt56SM1s6YQda5af";
 
 export const TalkToExpert = () => {
   return (
